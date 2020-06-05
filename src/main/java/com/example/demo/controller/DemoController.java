8c77e63a7deec9695f7742cf44eb2960aeefc744
Rajesh A P <aprajesh20101993@gmail.com>
2020-06-05 17:45:43 +0530

    Controller, properties, test
    Written DemoControllerTest for testing API that return String and a json value.

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,37 @@ public class DemoController {
 	@RequestMapping("/")
 	public String index() {
 		return "Hello World ! Greetings from Spring Boot!";
+	}
+
+	@GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Hello json() {
+		return new Hello("123", "Raj");
+	}
+
+	private class Hello {
+		private String id;
+		private String name;
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Hello(String id, String name) {
+			this.id = id;
+			this.name = name;
+		}
 	}
 
 }
