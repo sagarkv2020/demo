1191800afeb7957c9a199db112043f5a64086e92
Rajesh A P <aprajesh20101993@gmail.com>
2020-06-05 18:43:16 +0530

    update mediaType.

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.awt.PageAttributes.MediaType;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
@@ -8,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
