208e1e7537dea65d63915cf4d3bb239e95aae810
sagarkv2020 <sagarkv2020@gmail.com>
2020-06-05 12:24:59 +0530

    Commit test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Hello World ! Greetings from Spring Boot!";
 	}
 
 }
