069cd84a7d9c2caefd430f0d83c5ffa5ae1ea735
Aloke Tewary <aloketewary@gmail.com>
2020-06-12 18:29:52 +0530

    Removed unnecessary files from git

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -6,6 +6,8 @@ import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.model.Hello;
 import com.example.demo.service.AbstractResponse;
 import com.example.demo.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -18,12 +20,13 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@Api(value="employeeCtrl", description="Employee API endpoints")
 public class DemoController extends AbstractResponse {
 
     @Autowired
     private EmployeeService service;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Hello World ! Greetings from Spring Boot!";
     }
@@ -34,6 +37,7 @@ public class DemoController extends AbstractResponse {
     }
 
     @GetMapping("/employees")
+    @ApiOperation("Returns list of all Employees in the system.")
     public ResponseEntity<List<EmployeeResponseVO>> getAllEmployees() throws IOException, ParseException {
         List<EmployeeResponseVO> list = service.getAllEmployees();
         return successResponse(list);
